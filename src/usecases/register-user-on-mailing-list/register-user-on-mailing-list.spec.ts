be7a66f41b930d8b158a3f9149b576338bf6b866
Michael Vinicius <michaelvini62@gmail.com>
2020-12-20 19:33:37 -0300

    feat: user data interface

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from './user-data'
+
+describe('Register user on maling list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    //   const repo: UserRepository = new InMemoryUserRepository(users)
+    //   const usecase: RegisterUserOnaMalingList = new RegisterUserOnaMalingList(repo)
+    //   const name = 'any_name'
+    //   const email = 'any_email'
+    //   const response = await usecase.resgisterUserOnMalingList({ name, email })
+    //   const user = repo.findUserByEmail(email)
+    //   expect((await user).name).toBe('any_name')
+  })
+})
