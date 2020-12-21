af0e4a287bd56bae7979839041d47795c3d08875
Michael Vinicius <michaelvini62@gmail.com>
2020-12-20 21:28:54 -0300

    feat: return all users in the repository

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -17,4 +17,11 @@ describe('In memory user repository', () => {
     const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user.name).toBe('any_name')
   })
+
+  test('should return all users in the repository', async () => {
+    const users: UserData[] = [{ name: 'any_name', email: 'any@mail.com' }, { name: 'second@mail.com', email: 'any@mail.com' }]
+    const userRepo = new InMemoryUserRepository(users)
+    const returnedUsers = userRepo.findAllUsers()
+    expect((await returnedUsers).length).toBe(2)
+  })
 })
