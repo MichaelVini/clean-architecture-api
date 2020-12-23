0cd80c6c52365d4dc47e9c6df71b46be63d6882e
Michael Vinicius <michaelvini62@gmail.com>
2020-12-23 18:38:47 -0300

    feat: validade emails with more than 320 chars

--- src/usecases/register-user-on-mailing-list/entities/email-validation.spec.ts
+++ src/usecases/register-user-on-mailing-list/entities/email-validation.spec.ts
@@ -20,4 +20,9 @@ describe('Email validation', () => {
     const email = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept strings larger than 320 chars', () => {
+    const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
