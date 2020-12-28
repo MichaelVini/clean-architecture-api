6c46a3a60e30ed3e0f9de42ea6e093fbd8864cf1
Michael Vinicius <michaelvini62@gmail.com>
2020-12-28 16:10:29 -0300

    refactor: move user data to entities layer

---
+++ src/usecases/register-user-on-mailing-list/entities/user.spec.ts
@@ -0,0 +1,6 @@
+describe('User domain class', () => {
+  test('should not create user with invalid e-mail adress', () => {
+    // const invalidEmail = 'invalid_email'
+    // const error = User.create({ name: 'any_name', email: invalidEmail})
+  })
+})
