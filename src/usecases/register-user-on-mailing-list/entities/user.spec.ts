a720ae4c5a5b8f933899345c3edb029728b2ca2c
Michael Vinicius <michaelvini62@gmail.com>
2020-12-28 17:46:57 -0300

    test: verify e-mail address when creating a user

--- src/usecases/register-user-on-mailing-list/entities/user.spec.ts
+++ src/usecases/register-user-on-mailing-list/entities/user.spec.ts
@@ -1,6 +1,11 @@
+import { User } from './user'
+import { left } from '../shared/either'
+import { InvalidEmailError } from './errors/invalid-email-error'
+
 describe('User domain class', () => {
   test('should not create user with invalid e-mail adress', () => {
-    // const invalidEmail = 'invalid_email'
-    // const error = User.create({ name: 'any_name', email: invalidEmail})
+    const invalidEmail = 'invalid_email'
+    const error = User.create({ name: 'any_name', email: invalidEmail })
+    expect(error).toEqual(left(new InvalidEmailError()))
   })
 })
