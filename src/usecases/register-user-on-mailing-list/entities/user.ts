b7277f43c7c5c03070c05e73e2d836113f6f0917
Michael Vinicius <michaelvini62@gmail.com>
2021-01-20 19:01:46 -0300

    feat: create valid user

--- src/usecases/register-user-on-mailing-list/entities/user.ts
+++ src/usecases/register-user-on-mailing-list/entities/user.ts
@@ -1,11 +1,19 @@
 import { UserData } from './user-data'
-import { Either, left } from '../shared/either'
+import { Either, left, right } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { Email } from './email'
 import { InvalidNameError } from './errors/invalid-name-error'
 import { Name } from './name'
 
 export class User {
+  public readonly email: Email
+  public readonly name: Name
+
+  private constructor (name: Name, email: Email) {
+    this.name = name
+    this.email = email
+  }
+
   static create (UserData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const emailOrError = Email.create(UserData.email)
     const nameOrError = Name.create(UserData.name)
@@ -17,5 +25,10 @@ export class User {
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
   }
 }
